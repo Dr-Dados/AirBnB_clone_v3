baad4ae1bf18e1e0b84b431e230d320583df8131
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-29 20:58:47 +0000

    stats view

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,16 @@ the status of our API
 """
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -13,3 +23,12 @@ def status():
     return the status of the API
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stast():
+    """ return a Json for all classes and their count"""
+    count_dict = {}
+    for key, val in classes.items():
+        count_dict[key] = models.storage.count(val)
+    return jsonify(count_dict)
