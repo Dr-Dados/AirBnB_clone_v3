77b2e95b11a81bedfe5f62eb74274933923187a4
Sma3in96 <oumouss.ismail@gmail.com>
2024-03-29 16:06:35 +0000

    api setup

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" start flask app """
+from flask import Flask
+from models import storage
+from os import getenv
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_methode(args):
+    """ close the storage variable """
+    storage.close()
+
+
+if __name__ == '__main__':
+    API_host = getenv('HBNB_API_HOST') or "0.0.0.0"
+    API_port = int(getenv("HBNB_API_PORT") or 5000)
+    app.run(host=API_host, port=API_port, threaded=True)
