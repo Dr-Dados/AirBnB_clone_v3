f7f5351db711eb083a439e056e62313d79d21794
Dr-Dados <adnane.bachchar@gmail.com>
2024-03-31 18:34:32 +0000

    modify app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,46 +1,34 @@
 #!/usr/bin/python3
-"""Endpoint (route) will be to return the status of your API"""
-import os
-from flask import Flask
-from models import storage
+"""Falsk app."""
+
 from api.v1.views import app_views
-from flask_cors import CORS
+from flask import Flask
+from flask import make_response
+from flask import jsonify
+from models import storage
+import os
 
 
-# creating a Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
-app.register_blueprint(app_views, url_prefix="/api/v1")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return {"error": "Not found"}, 404
-
-
-@app.errorhandler(400)
-def page_not_found(e):
-    message = e.description
-    return message, 400
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close(ctx):
+def teardown_storage(exception):
+    """Close the storage connection at the end of the request."""
     storage.close()
 
 
-if os.getenv("HBNB_API_HOST"):
-    host = os.getenv("HBNB_API_HOST")
-else:
-    host = "0.0.0.0"
-
-if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT"))
-else:
-    port = 5000
+@app.errorhandler(404)
+def not_found(error):
+    """Handle the error response to retruns json instead of HTML."""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
+
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+
     app.run(host=host, port=port, threaded=True)
